12d72f7ddafd0a6aba6a68aedccf3dd596682694
victorgg1993 <vigomga1993@gmail.com>
2020-10-26 12:02:52 +0100

    añadido split y reset del split

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,6 +7,7 @@ let secDec = document.getElementById('secDec');
 let secUni = document.getElementById('secUni');
 let milDec = document.getElementById('milDec');
 let milUni = document.getElementById('milUni');
+let ol_split = document.getElementById('splits');
 
 
 function printTime(minutes, seconds, millis) {
@@ -34,6 +35,14 @@ function printMilliseconds(milis) {
 }
 
 function printSplit() {
+    let li_tmp = document.createElement('li');
+    li_tmp.innerText = `Tiempo: ${chronometer.minutes}:${chronometer.seconds}:${chronometer.millis}`;
+    //li_tmp.innerText = `Tiempo: ${}`;
+
+
+
+
+    ol_split.appendChild(li_tmp);
 
 }
 
@@ -79,18 +88,6 @@ btnLeft.addEventListener('click', function (e) {
         setStartBtn();
         setResetBtn();
     }
-
-    /*    e.currentTarget.classList.toggle('start');
-       e.currentTarget.classList.toggle('stop');
-       e.currentTarget.innerHTML ==='START'?
-       e.currentTarget.innerHTML = 'STOP':
-       e.currentTarget.innerHTML = 'START';
-       e.currentTarget.nextElementSibling.classList.toggle('reset');
-       e.currentTarget.nextElementSibling.classList.toggle('split');
-       e.currentTarget.nextElementSibling.innerHTML ==='RESET'?
-       e.currentTarget.nextElementSibling.innerHTML = 'SPLIT':
-       e.currentTarget.nextElementSibling.innerHTML = 'RESET'; */
-
 });
 
 // Reset/Split Button
@@ -98,15 +95,22 @@ btnRight.addEventListener('click', function () {
 
     //chronometer.setMilliseconds();
 
-    if (btnRight.innerText == 'RESET') {
-        minDec.innerText = 0;
-        minUni.innerText = 0;
-        secDec.innerText = 0;
-        secUni.innerText = 0;
-        milDec.innerText = 0;
-        milUni.innerText = 0;
+    switch (btnRight.innerText) {
+        case 'RESET':
+            minDec.innerText = 0;
+            minUni.innerText = 0;
+            secDec.innerText = 0;
+            secUni.innerText = 0;
+            milDec.innerText = 0;
+            milUni.innerText = 0;
+            chronometer.resetClick();
+            ol_split.innerHTML = '';
+            break;
 
-        chronometer.resetClick();
+        case 'SPLIT':
+            printSplit();
+            break;
+
     }
 
 });
